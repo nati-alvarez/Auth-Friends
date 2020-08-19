2f646032a3b16f79364088f0a4f1d6f3dbcbbe0a
Natividad Alvarez <tivialvarez@gmail.com>
2020-08-19 12:57:20 -0400

    Created ProtectedRoute component

---
+++ friends/src/ProtectedRoute.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Route, Redirect} from "react-router-dom";
+
+const ProtectedRoute = ({component: Component, ...rest}) =>{
+    console.log(...rest);
+    return(
+        <Route {...rest} render={props=>{
+            localStorage.getItem("token")? (
+                <Component {...props}/>
+            ): (
+                <Redirect to="/"/>
+            )
+        }}/>
+    )
+};
