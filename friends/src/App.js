25eaeca0c09100cb7bb51318fe26f74c76744341
Natividad Alvarez <tivialvarez@gmail.com>
2020-08-19 11:55:22 -0400

    Setup routing

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Switch, Route} from "react-router-dom";
 import './App.css';
+
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ friends/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <main className="home">
+            hello
+        </main>
+    )
+}
+
+export default Home;
