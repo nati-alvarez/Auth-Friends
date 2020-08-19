503e3e6de1f7754166e8382b0259e2352daec2c3
Natividad Alvarez <tivialvarez@gmail.com>
2020-08-19 12:32:27 -0400

    Set up reducer for friends data

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import {Switch, Route} from "react-router-dom";
 import './App.css';
+
+import reducer from "./reducers/index";
 
 import Home from "./components/Home";
 
 function App() {
+  const initialState = {
+    friends: []
+  }
+  
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
       <Switch>
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,6 @@
+export default function reducer(initialState, action){
+    switch(action.type){
+        default: 
+            return initialState;
+    }
+}
